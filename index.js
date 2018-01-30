6cb463d4e0ddc9de5d32ad4c774678f79595054e
Inomzhon Ibragimov <mail4inom@gmail.com>
2018-01-30 01:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function logWhisper(string){
 }
 function sayHiToGrandma(string){
   if (string.toLowerCase() === string) {
-     return "I can't hear you!"
+     return "i can't hear you!"
   }else if(string === string.toUpperCase()){
     return "YES INDEED!";
   }else{
